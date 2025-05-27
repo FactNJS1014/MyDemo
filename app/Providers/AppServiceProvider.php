8570ada5d31ee4292e46f6c5fdae3b73042e0e4f
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-05-27 14:49:09 +0700

    set provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,25 @@
 <?php
 
-namespace App\Providers;
-
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot()
     {
-        //
+        // Set log path to /tmp
+        Log::useFiles('/tmp/laravel.log');
+
+        // Set paths to tmp if running on Vercel
+        if (App::environment('production')) {
+            config([
+                'view.compiled' => '/tmp',
+                'cache.stores.file.path' => '/tmp',
+                'session.files' => '/tmp',
+            ]);
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+    public function register() {}
 }
